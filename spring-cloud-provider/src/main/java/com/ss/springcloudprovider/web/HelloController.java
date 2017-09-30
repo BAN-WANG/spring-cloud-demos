55560d82f80bf616710aaa9f0135107e625ff562
wsy <43888221@qq.com>
2017-09-30 10:53:40 +0800

    注册节点高可用模式

--- spring-cloud-provider/src/main/java/com/ss/springcloudprovider/web/HelloController.java
+++ spring-cloud-provider/src/main/java/com/ss/springcloudprovider/web/HelloController.java
@@ -1,14 +1,18 @@
 package com.ss.springcloudprovider.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
+    
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
+        LOG.info("/hello,name={}",name);
         return "hello "+name+"，this is first messge";
     }
 }
