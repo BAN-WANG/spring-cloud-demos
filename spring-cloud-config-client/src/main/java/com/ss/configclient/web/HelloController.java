244eb795ec180558205550d36399fa0634145ba8
wsy <43888221@qq.com>
2017-10-09 16:36:14 +0800

    spring cloud config使用

---
+++ spring-cloud-config-client/src/main/java/com/ss/configclient/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.ss.configclient.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: wsy
+ * \* Date: 2017/10/9
+ * \* Time: 15:53
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@RestController
+class HelloController {
+    @Value("${ss.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String getHello() {
+        return this.hello;
+    }
+}
